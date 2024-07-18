8c0783bfd4ab23202f72decd10216bf0f32b96bc
Sumit Yadav <sumitydv8416@gmail.com>
2024-07-18 18:59:38 +0530

    add homepage

--- django_project/settings.py
+++ django_project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
     #local
     'accounts.apps.AccountsConfig',
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -62,7 +63,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ pages/tests.py
@@ -0,0 +1,31 @@
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse, resolve
+
+from .views import HomePageView
+# Create your tests here.
+
+class HomepageTest(SimpleTestCase):
+
+    def setUp(self): 
+        url = reverse("home")
+        self.response = self.client.get(url)
+
+    def test_url_exist_at_correct_location(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    # def test_homepage_url_name(self):
+    #     response = self.client.get(reverse('home'))
+    #     self.assertEqual(response.status_code, 200)
+
+    def test_homepage_template(self): # new
+        self.assertTemplateUsed(self.response, "home.html")
+
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, 'home page')
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, 'hello')
+
+    def test_homepage_url_resolves_homepageview(self): # new
+        view = resolve("/")
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
